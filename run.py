5c2b18506c919090146d9af990b692f64d07cfde
AntonioJFdez <antoniojes.fernandez@gmail.com>
2025-06-06 19:08:57 +0200

    Update run.py

--- run.py
+++ run.py
@@ -138,9 +138,9 @@ def home():
 
 import os
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=10000)
+
 
 
 
